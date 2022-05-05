7c076111dedb7351852f7d1a156bebe2d927d924
thomasaturner <thomasaturner01@gmail.com>
2022-05-04 22:32:29 -0600

    slider updates and alert message updates

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear()+parseInt(years);  
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    if(principal == undefined || principal <= 0){
+        alert("Enter a Positive Number");
+        document.getElementById("principal").focus();
+        
+        return
+    }else{
+        // Display the text with the calculated values
+        var inr = "If you deposit <mark>" +principal+",</mark><br>";
+        var ra = "at an interest rate of <mark>"+rate+" %,</mark><br>";
+        var am="You will receive an amount of <mark>" +interest+ ",</mark><br>"
+        var ye ="in the year <mark>"+year +"</mark>";
+
+        document.getElementById("result").innerHTML=inr+ra+am+ye;
+
+    }
 }
 
-function updateRate()
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
-        
+
+function updateRate (){
+    document.getElementById("rate_val").innerText =
+    document.getElementById("rate").value;
+} 
